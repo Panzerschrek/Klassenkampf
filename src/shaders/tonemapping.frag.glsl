5a5dcacb6e2cea2392fc05c4181fa1ba611700bd
Panzerschrek <AntiPanzerschrek@yandex.ru>
2020-01-20 13:42:46 +0300

    Uproscenije šejdera.

--- src/shaders/tonemapping.frag.glsl
+++ src/shaders/tonemapping.frag.glsl
@@ -9,14 +9,15 @@ layout(location = 0) out vec4 out_color;
 void main()
 {
 	const vec3 k= vec3(9.0, 8.5, 8.0);
-	vec2 tex_coord_r= f_tex_coord * ((dot(f_tex_coord, f_tex_coord) + k.r) / (2.0 + k.r));
-	vec2 tex_coord_g= f_tex_coord * ((dot(f_tex_coord, f_tex_coord) + k.g) / (2.0 + k.g));
-	vec2 tex_coord_b= f_tex_coord * ((dot(f_tex_coord, f_tex_coord) + k.b) / (2.0 + k.b));
+	float scale_base= dot(f_tex_coord, f_tex_coord);
+	vec2 tex_coord_r= f_tex_coord * ((scale_base + k.r) * (0.5 / (2.0 + k.r))) + vec2(0.5, 0.5);
+	vec2 tex_coord_g= f_tex_coord * ((scale_base + k.g) * (0.5 / (2.0 + k.g))) + vec2(0.5, 0.5);
+	vec2 tex_coord_b= f_tex_coord * ((scale_base + k.b) * (0.5 / (2.0 + k.b))) + vec2(0.5, 0.5);
 
 	out_color=
 		vec4(
-			texture(tex, tex_coord_r * 0.5 + vec2(0.5, 0.5)).r,
-			texture(tex, tex_coord_g * 0.5 + vec2(0.5, 0.5)).g,
-			texture(tex, tex_coord_b * 0.5 + vec2(0.5, 0.5)).b,
+			texture(tex, tex_coord_r).r,
+			texture(tex, tex_coord_g).g,
+			texture(tex, tex_coord_b).b,
 			1.0);
 }
