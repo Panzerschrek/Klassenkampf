6677a1691ca6697a698834b9a4b0a7dfb463a51f
Panzerschrek <AntiPanzerschrek@yandex.ru>
2020-02-07 21:24:40 +0300

    Podderživajem takže nesžatyje formaty "DDS".

--- src/Game/DDSImage.cpp
+++ src/Game/DDSImage.cpp
@@ -16,10 +16,10 @@ struct DDSPixelFormat
 	uint32_t flags;
 	uint32_t fourCC;
 	uint32_t rgb_bit_count;
-	uint32_t r_bit_mask;
+	uint32_t r_bitmask;
 	uint32_t g_bitmask;
-	uint32_t b_bit_mask;
-	uint32_t a_bit_mask;
+	uint32_t b_bitmask;
+	uint32_t a_bitmask;
 };
 static_assert(sizeof(DDSPixelFormat) == 32u, "invalid size");
 
@@ -49,7 +49,8 @@ namespace DDSPixelFormatFlags
 		DDPF_ALPHAPIXELS= 0x01,
 		DDPF_ALPHA= 0x2,
 		DDPF_FOURCC= 0x04,
-		DDPF_RGB= 0x40;
+		DDPF_RGB= 0x40,
+		DDPF_RGBA= DDPF_RGB | DDPF_ALPHAPIXELS;
 }
 
 namespace DDSHeaderFlags
@@ -80,47 +81,66 @@ std::optional<DDSImage> DDSImage::Load(const char* const file_name)
 
 	// Determine format
 	vk::Format format= vk::Format::eUndefined;
-	size_t block_size= 0u;
-	if((header.pixel_format.flags & DDSPixelFormatFlags::DDPF_FOURCC) != 0)
+	size_t bits_per_pixel= 0u;
+	if((header.pixel_format.flags & DDSPixelFormatFlags::DDPF_FOURCC) == DDSPixelFormatFlags::DDPF_FOURCC)
 	{
 		if(std::memcmp(&header.pixel_format.fourCC, "DXT1", sizeof(uint32_t)) == 0u)
 		{
 			format= vk::Format::eBc1RgbaUnormBlock;
-			block_size= 8u;
+			bits_per_pixel= 4u;
 		}
 		if(std::memcmp(&header.pixel_format.fourCC, "DXT3", sizeof(uint32_t)) == 0u)
 		{
 			format= vk::Format::eBc2UnormBlock;
-			block_size= 16u;
+			bits_per_pixel= 8u;
 		}
 		if(std::memcmp(&header.pixel_format.fourCC, "DXT5", sizeof(uint32_t)) == 0u)
 		{
 			format= vk::Format::eBc3UnormBlock;
-			block_size= 16u;
+			bits_per_pixel= 8u;
 		}
 		if(std::memcmp(&header.pixel_format.fourCC, "BC4U", sizeof(uint32_t)) == 0u)
 		{
 			format= vk::Format::eBc4UnormBlock;
-			block_size= 8u;
+			bits_per_pixel= 4u;
 		}
 		if(std::memcmp(&header.pixel_format.fourCC, "BC4S", sizeof(uint32_t)) == 0u)
 		{
 			format= vk::Format::eBc4SnormBlock;
-			block_size= 8u;
+			bits_per_pixel= 4u;
 		}
 		if(std::memcmp(&header.pixel_format.fourCC, "ATI2", sizeof(uint32_t)) == 0u ||
 			std::memcmp(&header.pixel_format.fourCC, "BC5U", sizeof(uint32_t)) == 0u)
 		{
 			format= vk::Format::eBc5UnormBlock;
-			block_size= 16u;
+			bits_per_pixel= 8u;
 		}
 		if(std::memcmp(&header.pixel_format.fourCC, "BC5S", sizeof(uint32_t)) == 0u)
 		{
 			format= vk::Format::eBc5SnormBlock;
-			block_size= 16u;
+			bits_per_pixel= 8u;
 		}
 
 		// TODO - support other formats.
+	}
+	if((header.pixel_format.flags & DDSPixelFormatFlags::DDPF_RGBA) == DDSPixelFormatFlags::DDPF_RGBA)
+	{
+		if( header.pixel_format.r_bitmask == 0x000000FFu &&
+			header.pixel_format.g_bitmask == 0x0000FF00u &&
+			header.pixel_format.b_bitmask == 0x00FF0000u &&
+			header.pixel_format.a_bitmask == 0xFF000000u)
+		{
+			format= vk::Format::eR8G8B8A8Unorm;
+			bits_per_pixel= 32u;
+		}
+		if( header.pixel_format.r_bitmask == 0x00FF0000u &&
+			header.pixel_format.g_bitmask == 0x0000FF00u &&
+			header.pixel_format.b_bitmask == 0x000000FFu &&
+			header.pixel_format.a_bitmask == 0xFF000000u)
+		{
+			format= vk::Format::eB8G8R8A8Unorm;
+			bits_per_pixel= 32u;
+		}
 	}
 	if(format == vk::Format::eUndefined)
 		return std::nullopt;
@@ -142,7 +162,7 @@ std::optional<DDSImage> DDSImage::Load(const char* const file_name)
 		mip_levels[i].size_rounded[0]= (mip_levels[i].size[0] + 3u) & ~3u;
 		mip_levels[i].size_rounded[1]= (mip_levels[i].size[1] + 3u) & ~3u;
 
-		mip_levels[i].data_size= mip_levels[i].size_rounded[0] * mip_levels[i].size_rounded[1] / ((4u * 4u) / block_size);
+		mip_levels[i].data_size= mip_levels[i].size_rounded[0] * mip_levels[i].size_rounded[1] * bits_per_pixel / 8u;
 		offset+= mip_levels[i].data_size;
 	}
 
