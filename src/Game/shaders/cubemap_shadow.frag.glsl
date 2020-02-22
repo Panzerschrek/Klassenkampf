979a5adb69c4aa4ff6eb41d37bdc9d72fc34eab8
Panzerschrek <AntiPanzerschrek@yandex.ru>
2020-02-22 14:46:53 +0300

    Ispoljzujem konstantnoje smescenije dlä tenej.

--- src/Game/shaders/cubemap_shadow.frag.glsl
+++ src/Game/shaders/cubemap_shadow.frag.glsl
@@ -4,10 +4,11 @@ layout(location= 0) in vec3 f_pos; // World space position, relative to light so
 
 void main()
 {
+	const float c_constant_bias= 2.0 / 65536.0; // 2 values of 16-bit depth
 	const float c_bias_slope= 2.5;
 
 	float l= length(f_pos);
 	vec2 dd= vec2(dFdx(l), dFdy(l));
 
-	gl_FragDepth= l + c_bias_slope * length(dd);
+	gl_FragDepth= l + c_constant_bias + c_bias_slope * length(dd);
 }
