f5e3bd61a9cf361cd39b8ee8ac069815611b5b03
Panzerschrek <AntiPanzerschrek@yandex.ru>
2020-02-22 14:28:46 +0300

    Dobavläjem smescenije pri vycislenii glubiny v šejdere tenej.

--- src/Game/shaders/cubemap_shadow.frag.glsl
+++ src/Game/shaders/cubemap_shadow.frag.glsl
@@ -4,5 +4,11 @@ layout(location= 0) in vec3 f_pos; // World space position, relative to light so
 
 void main()
 {
-	gl_FragDepth= (length(f_pos) + 0.125) / 64.0f;
+	const float c_abs_bias= 1.0 / 64.0; // In world units.
+	const float c_bias_slope= 2.5;
+
+	float l= (length(f_pos) + c_abs_bias) / 64.0;
+	vec2 dd= vec2(dFdx(l), dFdy(l));
+
+	gl_FragDepth= l + c_bias_slope * length(dd);
 }
