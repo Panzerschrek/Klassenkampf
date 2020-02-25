0b74d4ad558bb1931cd4183713c6fd6131a97e80
Panzerschrek <AntiPanzerschrek@yandex.ru>
2020-02-25 12:40:51 +0300

    Ispoljzujem vse neobhodimyje dlä razmytija tekseli. Skladyvajem tekseli s koeffiçientami, blizkimi k krivoj gaussa. Ucityvajem sootnošenije storon ekrana.

--- src/Game/shaders/blur.frag.glsl
+++ src/Game/shaders/blur.frag.glsl
@@ -13,8 +13,23 @@ layout(location= 0) out vec4 color;
 
 void main()
 {
+	vec2 tex_size= vec2(textureSize(tex, 0));
+	int pixels_along_vector= int(dot(blur_vector.xy, tex_size));
+
 	vec4 r= vec4(0.0, 0.0, 0.0, 0.0);
-	for(int i= -5; i <= +5; ++i)
-		r+= texture(tex, f_tex_coord + float(i) * blur_vector.xy);
-	color= r * (1.0 / 11.0);
+	vec2 blur_vector_scaled= blur_vector.xy / float(pixels_along_vector);
+
+	const float c_exp_factor= -6.0;
+	float attenuation_factor= c_exp_factor / float(pixels_along_vector * pixels_along_vector);
+	float scale_factor_sub= exp2(c_exp_factor);
+
+	float scale_factor_accumulated= 0.0;
+	for(int i= -pixels_along_vector; i <= pixels_along_vector; ++i)
+	{
+		vec4 tex_value= texture(tex, f_tex_coord + float(i) * blur_vector_scaled);
+		float scale_factor= exp2(attenuation_factor * float(i*i)) - scale_factor_sub;
+		r+= scale_factor * tex_value;
+		scale_factor_accumulated+= scale_factor;
+	}
+	color= r / scale_factor_accumulated;
 }
