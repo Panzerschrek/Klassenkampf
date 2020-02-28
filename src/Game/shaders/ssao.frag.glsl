4e961a031d320255fc2dd000eb77dcb55b567f2d
Panzerschrek <AntiPanzerschrek@yandex.ru>
2020-02-28 11:35:19 +0300

    Ubrany tenevyje oreoly vokrug  objektov i šumy na ploskih poverhnostäh.

--- src/Game/shaders/ssao.frag.glsl
+++ src/Game/shaders/ssao.frag.glsl
@@ -71,8 +71,12 @@ void main()
 		vec2 sample_tex_coord= sample_screen_pos * 0.5 + vec2(0.5, 0.5);
 
 		float actual_sample_depth= texture(tex, sample_tex_coord).x;
-		occlusion_factor+= step(sample_depth, actual_sample_depth);
+		float actual_samle_w= view_matrix_values.w / (actual_sample_depth - view_matrix_values.z);
+
+		occlusion_factor+=
+			smoothstep(actual_samle_w, actual_samle_w + radius.x / 16.0, sample_world_pos.z) *
+			(1.0 - smoothstep(radius.x, radius.x * 1.5, sample_world_pos.z - actual_samle_w));
 	}
 
-	color= occlusion_factor / float(iterations);
+	color= 1.0 - occlusion_factor / float(iterations);
 }
