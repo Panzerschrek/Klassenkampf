ec1b83ae3319b3c4f0df1ee9c469c2bc26cb3380
Panzerschrek <AntiPanzerschrek@yandex.ru>
2020-02-28 14:48:43 +0300

    Razmyvajem s ucotom razniçy glubin.

--- src/Game/shaders/ssao_blur.frag.glsl
+++ src/Game/shaders/ssao_blur.frag.glsl
@@ -13,13 +13,25 @@ layout(location= 0) out float color;
 
 void main()
 {
-	float value= 0.0;
 	ivec2 coord= ivec2(gl_FragCoord.xy);
+
+	float frag_depth= texelFetch(depth_tex, coord, 0).x;
+	float frag_w= view_matrix_values.w / (frag_depth - view_matrix_values.z);
+
+	float value= 0.0;
+	float weights= 0.0;
+	float weight_factor= 1.0 / (2.0 * radius.x);
 	for(int dx= -1; dx <= 2; ++dx)
 	for(int dy= -1; dy <= 2; ++dy)
 	{
-		value+= texelFetch(ssao_tex, coord + ivec2(dx, dy), 0).x;
+		ivec2 sample_coord= coord + ivec2(dx, dy);
+		float sample_depth= texelFetch(depth_tex, sample_coord, 0).x;
+		float sample_w= view_matrix_values.w / (sample_depth - view_matrix_values.z);
+
+		float weight= 1.0 - min(1.0, abs(frag_w - sample_w) * weight_factor);
+		value+= weight * texelFetch(ssao_tex, sample_coord, 0).x;
+		weights+= weight;
 	}
 
-	color= value / 16.0;
+	color= value / weights;
 }
